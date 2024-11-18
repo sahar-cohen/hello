1f55e0c9a81141e89a0361da3f97315d8c2f57b2
sahar-cohen <saharc21@gmail.com>
2024-11-18 19:37:51 +0200

    trying more stuf

--- lecture1_functions.py
+++ lecture1_functions.py
@@ -1,7 +1,84 @@
+# def square(x):
+#     return x * x
+
+# # print(square(24))
+
+# Name = input ("Enter your name here ")
+
+# print (f"Your name is: {Name}")
+
+# def register():
+#     guestname = input("Guest name: ")
+#     print (f"The following guest: {guestname}, has been added to the list")
+
+# register()
+
+# guestlist = set()
+
+# while True:
+#     person = input("Guest name (or type 'done' to finish): ")
+#     if person.lower() == 'done':
+#         break
+    # guestlist.add(person)
+#     print(f"Current guest list: {guestlist}")
+
+# # print(guestlist)
+
+# # for person in guestlist:
+#     print(guestlist)
+
+
+# players = list()
+
+# while True:
+#     player = input("Player name (or type 'done' to finish): ")
+#     if player.lower() == 'done':
+#         break
+#     players.append(player)
+#     print(f"Current player list: {players}")
+
+# def add_number():
+#     int1 = int(input("Enter first number: "))
+#     int2 = int(input("Enter second number: "))
+#     return int1+int2
+
+# while True:  
+#     result = add_number()
+#     break
+    
+# print("The sum of the numbers is: ", result)
+
+# names = set()
+
+# while True:
+#     name = input("enter name here: ")
+#     names.add(name)
+#     names.add("Inbal")
+#     break
+
+# print(names)
+
+# def square ():
+#     x = int(input("Choose your number:"))
+#     result = x*x
+#     return result
+
+# result = square()
+
+# print("Your square surface is: " + str(result))
+
+
+# if result > 150:
+#     print("Try lower")
+# elif result < 100:
+#     print("Try higher")
+# elif result == 125:
+#     print("Nice work!")
+# else:
+#     print("Result is between 100 and 150.")
+
 def square(x):
     return x * x
 
-print(square(24))
-
-
+print()
 
